349af0edf58f2de6c0ea1584bc9085f98c168c3d
aleksandrskondratjevs <aleksandrs.kondratjevs@scandiweb.com>
2024-12-20 14:55:19 +0200

    Init setup of sharing header from Hyva to Luma

---
+++ app/code/Vendor/Theme/Block/Html/Header/Logo.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Vendor\Theme\Block\Html\Header;
+
+use Magento\Theme\Block\Html\Header\Logo as CoreLogo;
+
+class Logo extends CoreLogo
+{
+    /**
+     * Current template name
+     *
+     * @var string
+     */
+    protected $_template = 'Vendor_Theme::html/header/logo.phtml';
+}
